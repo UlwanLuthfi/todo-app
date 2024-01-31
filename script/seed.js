166f6c2ecbfb8082172e27e84a010afbe0276f0a
Ulwan Luthfi <luthfi030706@gmail.com>
2024-01-31 09:41:42 +0700

    Added placeholder-data.js and seed.js

---
+++ script/seed.js
@@ -0,0 +1,52 @@
+const { neon } = require("@neondatabase/serverless");
+const { todos } = require("../lib/placeholder-data.js");
+
+async function seedTodos(sql) {
+  try {
+    await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+    const createTable = await sql`
+    CREATE TABLE IF NOT EXISTS todos (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      priority VARCHAR(255) NOT NULL,
+      status VARCHAR(255) NOT NULL,
+      title VARCHAR(255) NOT NULL
+    );
+    `;
+
+    console.log(`Created "todos" table`);
+
+    const insertedTodos = await Promise.all(
+      todos.map(
+        (todo) => sql`
+        INSERT INTO todos (priority, status, title)
+        VALUES (${todo.priority}, ${todo.status}, ${todo.title})
+        ON CONFLICT (id) DO NOTHING;
+      `
+      )
+    );
+
+    console.log(`Seeded ${insertedTodos.length} todos`);
+
+    return {
+      createTable,
+      todo: insertedTodos,
+    };
+  } catch (error) {
+    console.error("Error seeding todos:", error);
+    throw error;
+  }
+}
+
+async function main() {
+  const sql = neon(process.env.DATABASE_URL);
+
+  await seedTodos(sql);
+}
+
+main().catch((err) => {
+  console.error(
+    "An error occurred while attempting to seed the database:",
+    err
+  );
+});
